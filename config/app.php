3dc7bc8402c1cbfa8fbf243c8c4e9b54af3268e1
Karina <129091450+karina725@users.noreply.github.com>
2023-04-23 17:40:37 -0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -54,7 +54,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    /*'url' => env('APP_URL', 'http://localhost'),*/
+    'url' => env('APP_URL', 'https://catalogo-movie.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
